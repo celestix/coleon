3021da6af28ca56f17fc789edd2529c9b5eb628e
celestix <73958752+celestix@users.noreply.github.com>
2023-10-02 15:54:48 +0530

    Modify run.bat and bring std::string into scope for flags

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir build
 cd build
-cmake -G "Unix Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ..
 make
 .\coleon.exe help
--- src/flags.h
+++ src/flags.h
@@ -1,3 +1,5 @@
+#include <iostream>
+
 enum FlagType {
     FlagString,
     FlagBool,
